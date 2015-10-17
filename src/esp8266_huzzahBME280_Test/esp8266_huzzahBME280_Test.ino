2e194224ea765e0dd0d4b2459276236493360b29
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-10-17 17:28:55 +0200

    experiment() : various experiments in "pseudo functional" C++ to make an RPM counter

--- src/esp8266_huzzahBME280_Test/esp8266_huzzahBME280_Test.ino
+++ src/esp8266_huzzahBME280_Test/esp8266_huzzahBME280_Test.ino
@@ -14,7 +14,57 @@ Adafruit_BME280 bme; // I2C
 //Adafruit_BME280 bme(BME_CS); // hardware SPI
 //Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
 
+
+struct RPMMeasurer {
+  int windRPM;
+  int curState;
+  int prevState;
+  
+  unsigned long startTime;
+  unsigned long currentTime;
+  unsigned long elapsedTime; 
+
+  RPMMeasurer():{
+    windRPM = 0;
+    curState = 0;
+    prevState= 0;
+  
+    startTime = millis();
+    currentTime = millis();
+    elapsedTime = 0;   
+  }
+  
+  //foo() : bar(3) {}   //look, a constructor
+  int measure(int state) 
+  { 
+    curState = state;
+    
+    if(curState != prevState){
+      currentTime = millis();
+      elapsedTime = currentTime - startTime;
+      windRPM =  60000/(time_between_pulses*pulses_per_rev)
+
+      //reset
+      prevState = state;
+      startTime = millis();
+      currentTime = millis();
+      
+      return windRPM
+  }
+};
+
+
+const int hallPin = 2; 
+const int ledPin =  0;    
+int hallState = 0; 
+
+RPMMeasurer rpm;
+
+
 void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(hallPin, INPUT);
+  
   Serial.begin(115200);
   Serial.println(F("BME280 test"));
 
@@ -25,7 +75,11 @@ void setup() {
   }
 }
 
+
+
 void loop() {
+    hallState = digitalRead(hallPin);
+    
     Serial.print("Temperature = ");
     Serial.print(bme.readTemperature());
     Serial.println(" *C");
@@ -43,7 +97,48 @@ void loop() {
     Serial.print(bme.readHumidity());
     Serial.println(" %");
 
+    Serial.print("Hall sensor state = ");
+    Serial.println(hallState);
+    
     Serial.println();
     delay(2000);
 }
- 
+
+
+
+int measureRPM(int state){
+  int windRPM = 0;
+  int curState = 0;
+  int prevState= 0;
+  
+  unsigned long startTime = millis();
+  unsigned long currentTime = millis();
+  unsigned long elapsedTime = 0; 
+
+  /*
+   * 60000/(time_between_pulses*pulses_per_rev)
+  (pulse_count/pulses_per_rev)*60
+  */
+  /*
+  int inner(int state){
+    curState = state;
+    
+    if(curState != prevState){
+      currentTime = millis();
+      elapsedTime = currentTime - startTime;
+      windRPM =  60000/(time_between_pulses*pulses_per_rev)
+
+      //reset
+      startTime = millis();
+      currentTime = millis();
+      return windRPM
+    }
+  }*/
+  //return [=](int state) mutable { return 42; };
+  //auto printlnB = [](const char  *message){ cout << message << std::endl;};
+  /*return [=]() mutable {
+      //cout << c++;
+  };*/
+
+}
+
