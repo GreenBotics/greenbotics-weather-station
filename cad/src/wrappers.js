706ee5667f20a1fdb5eab2a8ca3ea8535f796bc0
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-01-03 10:34:39 +0100

    feature(wrapper): - added curried translate rotate, mirror functions - added wrapped chain_hull - cleaned up and reorganized wrappers slightly

--- cad/src/wrappers.js
+++ cad/src/wrappers.js
@@ -1,5 +1,18 @@
-import {of, head, flatten} from 'ramda'
+import {of, head, flatten, curry, compose} from 'ramda'
 
+
+/*let greaterThan = curry( (limit, value) => value > limit )
+
+let greaterThanTwentyFive = greaterThan(25)
+console.log("A",greaterThan(25,20))
+console.log("B",greaterThanTwentyFive(26))*/
+
+
+/*let transforms = compose(
+  translate([20,10,5])
+)*/
+
+////
 
 function wrap(type){
   return function(options){
@@ -9,13 +22,14 @@ function wrap(type){
 
 function spreadWrap(type){
   return function(...options){
-    return of(type(flatten(options)))
+    return flatten( of(type(flatten(options))) )
   }
 }
 
-
-
 export function makeWraps(){
+  let _oldCube  = cube
+  cube   = wrap(_oldCube)
+
   let _oldSPhere = sphere
   sphere = wrap(_oldSPhere)
 
@@ -31,14 +45,47 @@ export function makeWraps(){
   let _oldHull = hull
   hull = spreadWrap(_oldHull)
 
+  let _oldChainHull = chain_hull
+  chain_hull = spreadWrap(_oldChainHull)
+
+  //booleans
   let _oldUnion = union
   union = spreadWrap( _oldUnion )
 
   let _oldDifference = difference
   difference = spreadWrap( _oldDifference )
 
+  //transforms
+  let _oldTranslate = translate
+  translate = curry( function(transform, operands){
+    //console.log("transform",transform, operands)
+    let ops = flatten( of(operands) )
+    return of( _oldTranslate( transform, ...ops ) )
+  } )
+
+  let _oldRotate = rotate
+  rotate = curry( function(transform, operands){
+    //console.log("transform",transform, operands)
+    let ops = flatten( of(operands) )
+    return of( _oldRotate( transform, ...ops ) )
+  } )
+
+
+  let _oldMirror = mirror
+  mirror = curry( function(transform, operands){
+    //console.log("transform",transform, operands)
+    let ops = flatten( of(operands) )
+    return of( _oldMirror( transform, ...ops ) )
+  } )
+
+  //curry( (transform, ...operands) => _oldTranslate(transform,operands) )
+
+
   return {
-    sphere, cylinder, square, circle,  hull, union, difference
+    cube, sphere, cylinder, 
+    square, circle,  
+    hull, chain_hull, union, difference, 
+    translate, rotate, mirror
   }
 
 }
