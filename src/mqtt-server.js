1158dcfeef9c27b4d706c347d527e35ef08e7f2e
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-21 23:18:09 +0100

    refactor(mqtt-server.js): updated/cleaned up server test

--- src/mqtt-server.js
+++ src/mqtt-server.js
@@ -1,72 +1,71 @@
 // Pulled directly from the MQTT example folder
 // https://github.com/adamvr/MQTT.js/blob/master/examples/server/broadcast.js
-var mqtt = require('mqtt')
-  , util = require('util');
+var mqtt = require('mqtt'),
+  util = require('util')
 
-mqtt.createServer(function(client) {
-  var self = this;
+mqtt.createServer(function (client) {
+  var self = this
 
-  if (!self.clients) self.clients = {};
+  if (!self.clients) self.clients = {}
 
+  client.on('connect', function (packet) {
+    self.clients[packet.clientId] = client
+    client.id = packet.clientId
+    console.log('client connected!', client.id)
+    client.subscriptions = []
+    client.connack({returnCode: 0})
+  })
 
-  client.on('connect', function(packet) {
-    self.clients[packet.clientId] = client;
-    client.id = packet.clientId;
-    console.log('client connected!', client.id);
-    client.subscriptions = [];
-    client.connack({returnCode: 0});
-  });
-
-  client.on('subscribe', function(packet) {
-    var granted = [];
+  client.on('subscribe', function (packet) {
+    var granted = []
 
     for (var i = 0; i < packet.subscriptions.length; i++) {
-      var qos = packet.subscriptions[i].qos
-        , topic = packet.subscriptions[i].topic
-        , reg = new RegExp(topic.replace('+', '[^\/]+').replace('#', '.+') + '$');
+      var qos = packet.subscriptions[i].qos,
+        topic = packet.subscriptions[i].topic,
+        reg = new RegExp(topic.replace('+', '[^\/]+').replace('#', '.+') + '$')
 
-      granted.push(qos);
-      client.subscriptions.push(reg);
+      granted.push(qos)
+      client.subscriptions.push(reg)
     }
 
-    client.suback({messageId: packet.messageId, granted: granted});
-  });
+    client.suback({messageId: packet.messageId, granted: granted})
+  })
 
-  client.on('publish', function(packet) {
+  client.on('publish', function (packet) {
     for (var k in self.clients) {
-      var c = self.clients[k];
+      var c = self.clients[k]
 
       for (var i = 0; i < c.subscriptions.length; i++) {
-        var s = c.subscriptions[i];
+        var s = c.subscriptions[i]
 
         if (s.test(packet.topic)) {
-          c.publish({topic: packet.topic, payload: packet.payload});
-          break;
+          c.publish({topic: packet.topic, payload: packet.payload})
+          break
         }
       }
     }
-  });
-
-  client.on('message',function(topic, message){
-    console.log("recieved message",topic, message)
   })
 
-  client.on('pingreq', function(packet) {
-    client.pingresp();
-  });
+  client.on('message', function (topic, message) {
+    console.log('recieved message', topic, message)
+  })
 
-  client.on('disconnect', function(packet) {
-    client.stream.end();
-  });
+  client.on('pingreq', function (packet) {
+    client.pingresp()
+  })
 
-  client.on('close', function(packet) {
-    delete self.clients[client.id];
-  });
+  client.on('disconnect', function (packet) {
+    client.stream.end()
+  })
 
-  client.on('error', function(e) {
-    client.stream.end();
-    console.log(e);
-  });
-}).listen(process.argv[2] || 1883);
+  client.on('close', function (packet) {
+    delete self.clients[client.id]
+  })
 
-console.log('listening for clients!');
+  client.on('error', function (e) {
+    client.stream.end()
+    console.log(e)
+  })
+}).listen(process.argv[2] || 1883)
+
+console.log('listening for clients!')
